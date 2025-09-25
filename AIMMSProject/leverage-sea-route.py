9df7a24349d6a3a50fd35e08e61c3fb204cfbde2
Chris Kuip <chris.kuip@aimms.com>
2025-09-25 16:05:47 +0200

    Added tooltips to the routes

--- AIMMSProject/leverage-sea-route.py
+++ AIMMSProject/leverage-sea-route.py
@@ -3,7 +3,7 @@ import pandas as pd
 from typing import Tuple
 from singleton import aimms_model, DataReturnTypes
 
-def searoute_work():
+def searoute_route():
     fromLon = aimms_model.p_origLon.data()
     fromLat = aimms_model.p_origLat.data()
     toLon   = aimms_model.p_destLon.data()
@@ -19,6 +19,9 @@ def searoute_work():
     # Pass Waypoints to AIMMS model
     route_columns = ['p_wayLon','p_wayLat']
     route_df = pd.DataFrame(route.geometry.coordinates,columns=route_columns)
+    route_len = len(route_df)
+    aimms_model.p_nowaypoints.assign(route_len)
+    
     # add column i_wayno
     route_df['i_wayno']=route_df.index
     # reorder columns, making i_wayno first.
@@ -28,3 +31,28 @@ def searoute_work():
     # and copying data to AIMMS.
     aimms_model.multi_assign(route_df,options={"return_type": DataReturnTypes.PANDAS})
 
+def searoute_distance_matrix():
+    # Getting data from AIMMS model:
+    distance_mat_list = []
+    df_location_overview = aimms_model.multi_data(["i_loc","p_latitude","p_longitude"])
+    location_overview_no_rows = len( df_location_overview )
+    for from_pos in range(location_overview_no_rows):
+        for to_pos in range(from_pos+1,location_overview_no_rows):
+            from_location_row = df_location_overview.iloc[from_pos]
+            to_location_row   = df_location_overview.iloc[  to_pos]
+            fromLoc =  from_location_row['i_loc']
+            fromLon =  from_location_row['p_longitude']
+            fromLat =  from_location_row['p_latitude']
+            toLoc   =  to_location_row['i_loc']
+            toLon   =  to_location_row['p_longitude']
+            toLat   =  to_location_row['p_latitude']
+            origin=[fromLon,fromLat]
+            destination=[toLon,toLat]
+            route = sr.searoute(origin,destination)
+            route_length = route.properties["length"]
+            route_tuple = (fromLoc, toLoc, route_length)
+            distance_mat_list.append( route_tuple )
+
+    distance_columns = ['i_loc_from','i_loc_to','p_distance_searoute']
+    distance_df = pd.DataFrame(distance_mat_list,columns=distance_columns)
+    aimms_model.p_distance_searoute.assign(distance_df)
