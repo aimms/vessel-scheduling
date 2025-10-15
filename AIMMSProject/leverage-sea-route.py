57e5bf311708dd448642a5af2fe1bdc89181e4c9
Chris Kuip <chris.kuip@aimms.com>
2025-10-15 12:38:19 +0200

    Created abstract procedure lpa::getRoute, such that routes can be obtained via an AIMMS procedure.

--- AIMMSProject/leverage-sea-route.py
+++ AIMMSProject/leverage-sea-route.py
@@ -31,6 +31,43 @@ def searoute_route(fromLat:float,fromLon:float,toLat:float,toLon:float):
     # and copying data to AIMMS.
     aimms_model.multi_assign(route_df,options={"return_type": DataReturnTypes.PANDAS})
 
+def test_scalar(parName:str):
+    df = pd.DataFrame({parName:[1234.5678]})
+    aimms_model.multi_assign(df,options={"return_type": DataReturnTypes.PANDAS})
+
+def searoute_route2(fromLat:float,fromLon:float,toLat:float,toLon:float,
+    indexName:str,latParName:str,lonParName:str,lenParName:str):
+    # fromLon = aimms_model.p_origLon.data()
+    # fromLat = aimms_model.p_origLat.data()
+    # toLon   = aimms_model.p_destLon.data()
+    # toLat   = aimms_model.p_destLat.data() 
+    origin=[fromLon,fromLat]
+    destination=[toLon,toLat]
+    route = sr.searoute(origin,destination)
+
+    # Pass total travel length to AIMMS model
+    route_length = route.properties["length"]
+    route_length_df = pd.DataFrame({lenParName:[route_length]})
+    aimms_model.multi_assign(route_length_df,options={"return_type": DataReturnTypes.PANDAS})
+
+    # Pass Waypoints to AIMMS model
+    route_columns = [lonParName,latParName]
+    route_df = pd.DataFrame(route.geometry.coordinates,columns=route_columns)
+    route_len = len(route_df)
+    # aimms_model.p_nowaypoints.assign(route_len) # test if needed.
+    
+    # add column i_wayno
+    route_df[indexName]=route_df.index
+    # reorder columns, making i_wayno first.
+    route_columns=route_df.columns.tolist()
+    route_columns=route_columns[-1:]+route_columns[:-1]
+    route_df=route_df[route_columns]
+    # and copying data to AIMMS.
+    aimms_model.multi_assign(route_df,options={"return_type": DataReturnTypes.PANDAS})
+    
+
+
+
 def searoute_distance_matrix():
     # Getting data from AIMMS model:
     distance_mat_list = []
